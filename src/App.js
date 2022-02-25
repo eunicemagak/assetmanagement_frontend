3dfbb45c12892e2d76af0feaec17ecefbd95812d
velvine <velvin.tanui@emalify.com>
2022-02-25 12:04:33 +0300

    added asset profile

--- src/App.js
+++ src/App.js
@@ -4,29 +4,34 @@ import './assets/css/global.css';
 import './assets/css/sidebar.css';
 import './assets/css/admin.css';
 import './assets/css/nav.css'
+import './assets/css/login.css'
+import './assets/css/landing.css'
+import './assets/css/assetprofile.css'
 import Admindash from './components/Admindash';
 import Users from './components/Users';
 import Assets from './components/Assets';
 import Complaints from './components/Complaints';
-import Sidebar from './components/Sidebar';
-import Nav from './components/Nav';
+import Assetprofile from './components/Assetprofile';
+//import Landing from './components/Landing';
+
 
 const App = () => {
+
+  
   return (
-    <div className='wrapper'>
+    //<Route path ='/' element={<Landing/>} />
       <Router>
-          <Nav />
-          <Sidebar/>
-      <div className='bg'>
         <Routes>
-          <Route path='/dashboard' element={<Admindash/>} />
+          <Route path='/admindash' element={<Admindash/>} />
           <Route path='/users' element={<Users/>} />
           <Route path='/assets' element={<Assets/>} />
           <Route path='/complaints' element={<Complaints/>} />
+          <Route path='/' element={<Assetprofile/>} />
         </Routes>
-      </div>
+  
       </Router>
-    </div>
+  
+
   )
 }
 
--- src/components/Admindash.js
+++ src/components/Admindash.js
@@ -1,43 +1,49 @@
 import React from 'react'
+import Sidebar from './Sidebar';
+import Nav from './Nav';
 
 const admindash = () => {
   return (
-        <div className='main'>
-          <div className='welcome'>
-            <h2>WELCOME JUMA</h2>
-            <p>Use this asset management portal to manage, assign and record all company assets for better convinience</p>
-          </div>
-          <div className='widget'>
-            <div className='data'>
-              <h1>36</h1>
-              <p>total assets</p>
-            </div>
-            <img className='widget-img' src={require("../assets/images/laptop.png")} alt=''></img>
-          </div>
-          <div className='widget'>
-            <div className='data'>
-              <h1>24</h1>
-              <p>assigned assets</p>
-            </div>
-            <img className='widget-img' src={require("../assets/images/assigned.png")} alt=''></img>
-          </div>
-          <div className='widget'>
-            <div className='data'>
-              <h1>12</h1>
-              <p>unassigned assets</p>
-            </div>
-            <img className='widget-img' src={require("../assets/images/unassigned.png")} alt=''></img>
-          </div>
-          <div className='widget'>
-            <div className='data'>
-              <h1>4</h1>
-              <p>new complaints</p>
-            </div>
-            <img className='widget-img' src={require("../assets/images/complaints.png")} alt=''></img>
-          </div>
-          
+    <div>
+      <Nav />
+      <Sidebar />
+      <div className='main'>
+        <div className='welcome'>
+          <h2>WELCOME JUMA</h2>
+          <p>Use this asset management portal to manage, assign and record all company assets for better convinience</p>
         </div>
-  )
+        <div className='widget'>
+          <div className='data'>
+            <h1>36</h1>
+            <p>total assets</p>
+          </div>
+          <img className='widget-img' src={require("../assets/images/laptop.png")} alt=''></img>
+        </div>
+        <div className='widget'>
+          <div className='data'>
+            <h1>24</h1>
+            <p>assigned assets</p>
+          </div>
+          <img className='widget-img' src={require("../assets/images/assigned.png")} alt=''></img>
+        </div>
+        <div className='widget'>
+          <div className='data'>
+            <h1>12</h1>
+            <p>unassigned assets</p>
+          </div>
+          <img className='widget-img' src={require("../assets/images/unassigned.png")} alt=''></img>
+        </div>
+        <div className='widget'>
+          <div className='data'>
+            <h1>4</h1>
+            <p>new complaints</p>
+          </div>
+          <img className='widget-img' src={require("../assets/images/complaints.png")} alt=''></img>
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
 export default admindash
